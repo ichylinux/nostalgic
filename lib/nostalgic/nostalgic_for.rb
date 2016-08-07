6f67fe964fd66f2238eb220eb26c635819263c81
ichy <ichylinux@gmail.com>
2016-08-07 20:22:49 +0900

    refactoring

--- lib/nostalgic/nostalgic_for.rb
+++ lib/nostalgic/nostalgic_for.rb
@@ -7,8 +7,8 @@ module Nostalgic
       def nostalgic_for(*attrs)
         attrs.each do |attr|
           class_eval <<-METHODS, __FILE__, __LINE__ + 1
-            def #{attr}_at(datetime = nil)
-              return self.#{attr} if datetime.nil?
+            def #{attr}_at(datetime)
+              raise ArgumentError.new if datetime.nil?
 
               na = Nostalgic::Attr.where(:type => self.class.name, :model_id => self.id, :name => '#{attr}')
               na = na.where('effective_at <= ?', datetime).order('effective_at desc').first
--- test/dummy/test/models/user_test.rb
+++ test/dummy/test/models/user_test.rb
@@ -6,6 +6,10 @@ class UserTest < ActiveSupport::TestCase
     assert @user = User.first
     assert_equal '222-123-4567', @user.tel
 
+    assert_raise ArgumentError do
+      @user.tel_at(nil)
+    end
+
     assert_equal '111-123-4567', @user.tel_at(Date.today - 1.day)
     assert_equal '222-123-4567', @user.tel_at(Date.today)
     assert_equal '333-123-4567', @user.tel_at(Date.today + 1.day)
