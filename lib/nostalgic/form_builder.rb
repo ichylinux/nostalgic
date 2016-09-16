e443c1d765a0c8adaef0b0940734d4835fd1bacc
ichy <ichylinux@gmail.com>
2016-09-16 15:50:08 +0900

    add form_helper method 'nostalgic_text_field'

--- lib/nostalgic/form_builder.rb
+++ lib/nostalgic/form_builder.rb
@@ -4,19 +4,7 @@ class ActionView::Helpers::FormBuilder
   include ActionView::Helpers::AssetTagHelper
 
   def nostalgic_text_field(method, options = {})
-    text_field(method, append_nostalgic_clsss(options))
-  end
-
-  def nostalgic_number_field(method, options = {})
-    number_field(method, append_nostalgic_clsss(options))
-  end
-
-  private
-
-  def append_nostalgic_clsss(options = {})
-    klass = options.fetch(:class, '').split
-    klass.unshift('nostalgic')
-    options.merge(:class => klass.join(' '))
+    @template.render 'nostalgic/text_field', :f => self, :attr => method, :css_class => options[:class]
   end
 
 end
--- nostalgic.gemspec
+++ nostalgic.gemspec
@@ -28,6 +28,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency 'bundler', '~> 1.12'
   s.add_development_dependency 'minitest', '~> 5.0'
-  s.add_development_dependency 'rake', '~> 10.0'
+  s.add_development_dependency 'rake', '~> 11.0'
   s.add_development_dependency 'sqlite3'
 end
