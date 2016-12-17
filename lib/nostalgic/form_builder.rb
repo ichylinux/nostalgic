b8144e497a7b3b8f99fd54c37ef547579e9b832a
ichy <ichylinux@gmail.com>
2016-12-17 13:35:54 +0900

    collection_select

--- lib/nostalgic/form_builder.rb
+++ lib/nostalgic/form_builder.rb
@@ -7,4 +7,10 @@ class ActionView::Helpers::FormBuilder
     @template.render 'nostalgic/text_field', :f => self, :attr => method, :css_class => options[:class]
   end
 
+  def nostalgic_collection_select(method, collection, value_method, text_method, options = {}, html_options = {})
+    @template.render 'nostalgic/collction_select',
+        :f => self, :attr => method, :collection => collection, :value_method => value_method, :text_method => text_method,
+        :options => options, :html_options => html_options
+  end
+
 end
