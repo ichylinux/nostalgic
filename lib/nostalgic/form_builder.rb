076223effdd85dbdcab9fbe19b28dc77d49c5dc0
ichy <ichylinux@gmail.com>
2020-01-18 19:33:13 +0900

    refactor

--- lib/nostalgic/form_builder.rb
+++ lib/nostalgic/form_builder.rb
@@ -4,13 +4,13 @@ class ActionView::Helpers::FormBuilder
   include ActionView::Helpers::AssetTagHelper
 
   def nostalgic_text_field(method, options = {})
-    @template.render 'nostalgic/text_field', :f => self, :attr => method, :css_class => options[:class]
+    @template.render 'nostalgic/text_field', f: self, attr: method, css_class: options[:class]
   end
 
   def nostalgic_collection_select(method, collection, value_method, text_method, options = {}, html_options = {})
     @template.render 'nostalgic/collection_select',
-        :f => self, :attr => method, :collection => collection, :value_method => value_method, :text_method => text_method,
-        :options => options, :html_options => html_options
+        f: self, attr: method, collection: collection, value_method: value_method, text_method: text_method,
+        options: options, html_options: html_options
   end
 
 end
